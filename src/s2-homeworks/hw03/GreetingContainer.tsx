3e9aaf2e31bdd42b530475faa75a2c547be092db
Olga Evtikhova <olga.evtikhova@gmail.com>
2026-09-20 22:34:12 +0300

    deploy

---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import Greeting from './Greeting'
+import { UserType } from './HW3'
+
+type GreetingContainerPropsType = {
+    users: UserType[]
+    addUserCallback: (name: string) => void
+}
+
+export const pureAddUser = (
+    name: string,
+    setError: (error: string) => void,
+    setName: (name: string) => void,
+    addUserCallback: (name: string) => void
+) => {
+    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+    if (!name.trim()) {
+        setError('Ошибка! Введите имя!')
+    } else {
+        addUserCallback(name.trim())
+        setName('')
+        setError('')
+    }
+}
+
+export const pureOnBlur = (name: string, setError: (error: string) => void) => {
+    // если имя пустое - показать ошибку
+    if (!name.trim()) {
+        setError('Ошибка! Введите имя!')
+    }
+}
+
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => {
+    // если нажата кнопка Enter - добавить
+    if (e.key === 'Enter') {
+        addUser()
+    }
+}
+
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+        error && setError('')
+    }
+
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = users.length
+    const lastUserName = users.length > 0 ? users[users.length - 1].name : ''
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
---
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { v1 } from 'uuid'
+import s2 from '../../s1-main/App.module.css'
+import GreetingContainer from './GreetingContainer'
+
+// types
+export type UserType = {
+    _id: string
+    name: string
+}
+
+export const pureAddUserCallback = (name: string, setUsers: (users: UserType[]) => void, users: UserType[]) => {
+    const user = {
+        _id: v1(),
+        name: name.trim() // лучше убирать пробелы по краям
+    }
+    setUsers([...users, user])
+}
+
+const HW3 = () => {
+    const [users, setUsers] = useState<UserType[]>([])
+
+    const addUserCallback = (name: string) => {
+        pureAddUserCallback(name, setUsers, users)
+    }
+
+    return (
+        <div id={'hw3'}>
+            <div className={s2.hwTitle}>Homework #3</div>
+            {/*для автоматической проверки дз (не менять)*/}
+
+            <div className={s2.hw}>
+                <GreetingContainer
+                    users={users}
+                    addUserCallback={addUserCallback}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HW3
